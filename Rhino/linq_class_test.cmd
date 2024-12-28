b12d91ad9511789ae4c5b93a511c6b7837bb52da
Stefan Schnell <130429818+StSchnell@users.noreply.github.com>
2024-12-28 06:48:12 +0100

    Update linq_class_test.cmd
    Update

--- Rhino/linq_class_test.cmd
+++ Rhino/linq_class_test.cmd
@@ -2,8 +2,8 @@
 REM Begin---------------------------------------------------------------
 
 REM This is the current version
-java -cp ".;rhino-1.7.14.jar" org.mozilla.javascript.tools.jsc.Main linq_class_test.js
-java -cp ".;rhino-1.7.14.jar" linq_class_test
+java -cp ".;rhino-1.7.15.jar" org.mozilla.javascript.tools.jsc.Main linq_class_test.js
+java -cp ".;rhino-1.7.15.jar" linq_class_test
 
 REM This is a really old version but used in the context of VMware Aria Automation
 REM java -cp ".;rhino-1.7R4.jar" org.mozilla.javascript.tools.jsc.Main linq_class_test.js
